ba639957983d14b80a148e299c444d483bfc94dc
Sia Chen Ren <siachenren@gmail.com>
2021-04-12 12:45:49 +0800

    Fix/update electron builder (#3)

--- src/preload.ts
+++ src/preload.ts
@@ -2,5 +2,5 @@ import { ipcRenderer } from "electron";
 
 ipcRenderer.on("upload-form", (_event, msg) => {
   const base64 = window.btoa(msg);
-  window.postMessage({ type: "LOAD_DOCUMENT", payload: base64 }, null);
+  window.postMessage({ type: "NESTED_DOCUMENT_LOAD", payload: base64 }, null);
 });
