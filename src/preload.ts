aac4db2f1f1dfac72759d0fb847fe1a057ef13e7
Chen Ren <siachenren@gmail.com>
2021-04-12 10:56:20 +0800

    fix: removed alert

--- src/preload.ts
+++ src/preload.ts
@@ -1,7 +1,6 @@
 import { ipcRenderer } from "electron";
 
 ipcRenderer.on("upload-form", (_event, msg) => {
-  alert("Preload");
   const base64 = window.btoa(msg);
   window.postMessage({ type: "NESTED_DOCUMENT_LOAD", payload: base64 }, null);
 });
